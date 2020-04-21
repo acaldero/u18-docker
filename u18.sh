b0fa5313781a2aed2247b452ede401172b07a25b
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2020-04-21 22:10:49 +0200

    minor update

--- u18.sh
+++ u18.sh
@@ -16,11 +16,15 @@ fi
 # for each argument, try to execute it
 #
 
+. ./.env
+
+CO_NAME=$COMPOSE_PROJECT_NAME"_node_1"
+
 for arg_i in "$@"
 do
 	case $arg_i in
 	     build)
-		docker image build -t u18 -f u18-dockerfile .
+		docker image build -f u18-dockerfile .
 	     ;;
 	     start)
 		docker-compose -f u18-dockercompose.yml up -d
@@ -29,7 +33,7 @@ do
 		docker ps
 	     ;;
 	     bash)
-		docker exec -it u18 /bin/bash
+		docker exec -it $CO_NAME /bin/bash
 	     ;;
 	     stop)
 		docker-compose -f u18-dockercompose.yml down
